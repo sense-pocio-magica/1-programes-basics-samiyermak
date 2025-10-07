fdd0677e1c10c6bae8304a54d5cba444f0adccd7
samiyermak <yksamigithub@gmail.com>
2025-10-07 03:35:28 +0200

    sami

--- exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
+++ exercicis/exercici1/obj/Debug/net9.0/exercici1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09f4a818ab4068e8cfdf00e84505a0979dfdcad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f2e7e25fb36f81afa75f21887eb262b6abb8b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercici1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
